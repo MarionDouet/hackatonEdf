a74d15fe412c618c306df4b4e3206f423a1550bc
Marion Douet <squalaladraw@gmail.com>
2019-12-19 15:23:46 +0100

    ajout form

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Formu from './components/Formu'
+
+function App() {
+  return (
+    <div className="App">
+     <Formu />
+    </div>
+  );
+}
+
+export default App;
