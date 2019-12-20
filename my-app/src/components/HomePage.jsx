47ba088efce95e4444cc701045f530f1b317fa16
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-12-20 11:55:22 +0100

    yoo

--- my-app/src/components/HomePage.jsx
+++ my-app/src/components/HomePage.jsx
@@ -22,95 +22,95 @@ const HomePage = (props) => {
 
   return (
     <div>
-    <div className="header-container">
-      <img src={logoPrincipal} className="logoPrincipal" />
-      <div>
-        <Nav tabs>
-          <NavItem className="item-nav">
-            <NavLink
-              className={classnames({ active: activeTab === '1' })}
-              onClick={() => { toggle('1'); }}
-            >
-              Ce qu'il est
+      <div className="header-container">
+        <img src={logoPrincipal} className="logoPrincipal" />
+        <div>
+          <Nav tabs>
+            <NavItem className="item-nav">
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => { toggle('1'); }}
+              >
+                Ce qu'il est
         </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === '2' })}
-              onClick={() => { toggle('2'); }}
-            >
-              Aujourd'hui
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => { toggle('2'); }}
+              >
+                Aujourd'hui
         </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === '3' })}
-              onClick={() => { toggle('3'); }}
-            >
-              Et demain?
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '3' })}
+                onClick={() => { toggle('3'); }}
+              >
+                Et demain?
         </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === '4' })}
-              onClick={() => { toggle('4'); }}
-            >
-              Map
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '4' })}
+                onClick={() => { toggle('4'); }}
+              >
+                Map
         </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === '5' })}
-              onClick={() => { toggle('5'); }}
-            >
-              Contact
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '5' })}
+                onClick={() => { toggle('5'); }}
+              >
+                Contact
         </NavLink>
-          </NavItem>
-        </Nav>
-      </div>
+            </NavItem>
+          </Nav>
+        </div>
 
+
+      </div>
+      <TabContent className="fond" activeTab={activeTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col sm="12">
+              <Environnement />
+              <Text />
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col sm="12">
+              <AboutUs />
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="3">
+          <Row>
+            <Col sm="12">
+              <MapTest />
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="4">
+          <Row>
+            <Col sm="12">
+              <Projet />
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="5">
+          <Row>
+            <Col sm="12">
+              <Contact />
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
 
     </div>
-    <TabContent className="fond" activeTab={activeTab}>
-      <TabPane tabId="1">
-        <Row>
-          <Col sm="12">
-            <Environnement />
-            <Text />
-          </Col>
-        </Row>
-      </TabPane>
-      <TabPane tabId="2">
-        <Row>
-          <Col sm="12">
-            <AboutUs />
-          </Col>
-        </Row>
-      </TabPane>
-      <TabPane tabId="3">
-        <Row>
-          <Col sm="12">
-            <Projet />
-          </Col>
-        </Row>
-      </TabPane>
-      <TabPane tabId="4">
-        <Row>
-          <Col sm="12">
-            <MapTest/>
-          </Col>
-        </Row>
-      </TabPane>
-      <TabPane tabId="5">
-        <Row>
-          <Col sm="12">
-            <Contact />
-          </Col>
-        </Row>
-      </TabPane>
-    </TabContent>
-   
-  </div>
   );
 }
 
